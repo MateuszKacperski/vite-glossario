c94226675039ec565b966b4174e1bac1f4d254fb
Mateusz Kacperski <kacperskimateusz95@gmail.com>
2024-04-08 09:57:24 +0200

    added app header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
+            <div class="container-fluid">
+                <RouterLink class="navbar-brand" to="/">Glossario</RouterLink>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <RouterLink class="nav-link" to="/">Home</RouterLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+<style lang='scss' scoped>
+/* style here */
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
-/web routes
+//web routes
 import { createRouter, createWebHistory } from "vue-router";
 //importo le pagine che mi servono
 import HomePage from "../components/pages/HomePage.vue";
-import ContactUsPage from "../components/pages/ContactUsPage.vue";
 import NotFoundPage from "../components/pages/NotFoundPage.vue";
-import ProjectDetailPage from "../components/pages/ProjectDetailPage.vue";
-import TypeProjectsPage from "../components/pages/TypeProjectsPage.vue";
+import WordDetailPage from "../components/pages/WordDetailPage.vue";
+
 //definisco le rotte
 const router = createRouter({
   history: createWebHistory(),
@@ -14,16 +13,10 @@ const router = createRouter({
 routes: [
     { path: "/", component: HomePage, name: "home" },
     {
-      path: "/projects/:slug",
-      component: ProjectDetailPage,
-      name: "project-detail",
+      path: "/words/:id",
+      component: WordDetailPage,
+      name: "word-detail",
     },
-    {
-      path: "/types/:slug/projects",
-      component: TypeProjectsPage,
-      name: "type-projects",
-    },
-    { path: "/contact-us", component: ContactUsPage, name: "contact-us" },
     { path: "/not-found", component: NotFoundPage, name: "not-found" },
     { path: "/:pathMatch(.*)*", redirect: "not-found" },
   ],
