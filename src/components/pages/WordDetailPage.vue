0413357fbfe52d46db04a33b34e85ea1d551ca84
ariannacostanzo <costanzo.arianna@outlook.it>
2024-04-08 10:46:22 +0200

    created word detail page

--- src/components/pages/WordDetailPage.vue
+++ src/components/pages/WordDetailPage.vue
@@ -1,13 +1,39 @@
 <script>
+import axios from 'axios';
+const defaultUrl = 'http://localhost:8000/api/words/'
   export default {
-    name: ''
+    name: 'WordDetailPage',
+    data() {
+      return {
+        word: null
+      }
+    },
+    methods: {
+      fetchWord(){
+        axios.get(defaultUrl + this.$route.params.id)
+        .then((res) => {
+          this.word = res.data
+        })
+      }
+    },
+    created() {
+      this.fetchWord()
+    }
   }
 </script>
 
 <template>
-  dettaglio
+  <div class="card  mb-3">
+    <div v-if="word" class="card-body">
+      <h5 class="card-title">{{ word.term }}</h5>
+      <p class="card-text">{{ word.definition }}</p>
+      <button @click="$router.back()" class="btn btn-primary">Torna indietro</button>
+    </div>
+  </div>
 </template>
 
 <style lang='scss' scoped>
-  /* style here */
+.card-title {
+  text-transform: capitalize;
+}
 </style>
